3a40c3b8c886bf49717b061a4ddc27a9298c1d68
Raja Omer Mustafa <o.mustafa.pseb@gmail.com>
2025-08-11 15:02:57 +0500

    galary removed

--- src/views/mj/aiSider.vue
+++ src/views/mj/aiSider.vue
@@ -68,7 +68,7 @@ const chatId= computed(()=>chatStore.active??'1002' );
             </a> -->
 
 
-            <a v-if="!isDisableMenu ( 'draws')"  @click="st.active='draw'; urouter.push(`/draw`)" class=" router-link-exact-active h-12 w-12 cursor-pointer rounded-xl bg-white duration-300 dark:bg-[#34373c] hover:bg-[#bbb] dark:hover:bg-[#555]">
+            <!-- <a v-if="!isDisableMenu ( 'draws')"  @click="st.active='draw'; urouter.push(`/draw`)" class=" router-link-exact-active h-12 w-12 cursor-pointer rounded-xl bg-white duration-300 dark:bg-[#34373c] hover:bg-[#bbb] dark:hover:bg-[#555]">
                 <n-tooltip placement="right" trigger="hover">
                   <template #trigger> 
                     <div  class="flex h-full justify-center items-center   py-1 flex-col" :class="[goHome=='draw' ? 'active' : '']">
@@ -78,11 +78,11 @@ const chatId= computed(()=>chatStore.active??'1002' );
                   </template>
                     {{$t('mjtab.drawinfo')}}
                 </n-tooltip>
-            </a>
+            </a> -->
 
 
 
-             <a  v-if="!isDisableMenu ( 'gallery')"  @click="homeStore.setMyData({act:'gallery'}) " class=" router-link-exact-active h-12 w-12 cursor-pointer rounded-xl bg-white duration-300 dark:bg-[#34373c] hover:bg-[#bbb] dark:hover:bg-[#555]">
+             <!-- <a  v-if="!isDisableMenu ( 'gallery')"  @click="homeStore.setMyData({act:'gallery'}) " class=" router-link-exact-active h-12 w-12 cursor-pointer rounded-xl bg-white duration-300 dark:bg-[#34373c] hover:bg-[#bbb] dark:hover:bg-[#555]">
                 <n-tooltip placement="right" trigger="hover">
                   <template #trigger> 
                     <div  class="flex h-full justify-center items-center   py-1 flex-col" >
@@ -92,7 +92,7 @@ const chatId= computed(()=>chatStore.active??'1002' );
                   </template>
                     {{ $t('mjtab.galleryInfo') }}
                 </n-tooltip>
-            </a>
+            </a> -->
 
 
             <a v-if="!isDisableMenu ( 'music')"      @click="st.active='music'; urouter.push('/music')" class=" router-link-exact-active h-12 w-12 cursor-pointer rounded-xl bg-white duration-300 dark:bg-[#34373c] hover:bg-[#bbb] dark:hover:bg-[#555]"
